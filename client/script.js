747c2051f6fc9f77b2a3ea64294dd41337ecaa1e
sangay-yonten <sangay9yonten@gmail.com>
2023-03-17 19:39:54 +0600

    update server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     //fetch data from the server -> bot's response
-    const response = await fetch('http://localhost:4200', {
+    const response = await fetch('https://openai-codex-57kl.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
