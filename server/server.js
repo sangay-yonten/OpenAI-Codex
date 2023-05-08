4b2124605b7c51b5392b176807ad207361a21e3d
Sangay Yonten <sangay9yonten@gmail.com>
2023-05-08 17:52:36 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -31,9 +31,9 @@ app.post('/', async(req, res) => {
             model: "text-davinci-003",
             prompt: `${prompt}`,
             temperature: 0,
-            max_tokens: 3000,
+            max_tokens: 2048,
             top_p: 1,
-            frequency_penalty: 0.5,
+            frequency_penalty: 0,
             presence_penalty: 0
         });
         res.status(200).send({
